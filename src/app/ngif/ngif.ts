cdce051359a2fe0a5b9099c2a18f5fd9dfab6587
srlnnodejs <srlnnodejs@gmail.com>
2019-12-17 15:40:59 +0530

    some task added

--- src/app/ngif/ngif.ts
+++ src/app/ngif/ngif.ts
@@ -6,7 +6,15 @@ templateUrl:"./ngif.html"
 export class NgIf{
 title="welcome srlnp this is ngif component"
 array_one:any[]
+array_two:any[]
 ngOnInit() {
-    this.array_one=["srlnp","subham","sangam"]
+    this.array_one=["srlnp","subham","sangam"];
+    this.array_two=[{"name":"srlnp","city":"berhampur"},
+    {"name":"sukumar","city":"puri"},
+    {"name":"sanjay","city":"aska"},
+    {"name":"sonali","city":"balugaon"},
+    {"name":"kumari","city":"aska"},
+    {"name":"tunguru","city":"mungrai"},
+    ]
 }
 }
